f5a0258a53aae18ab6eabce1c7675cab45ec13f9
Tiesnesis <ulvisgoldbergs@gmail.com>
2016-03-07 00:32:35 +0200

    Change server structure to object oriented solution

---
+++ Server/Server/BackBone.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using Server;
+
+namespace Server
+{
+    class BackBone
+    {
+
+        int userCounter = 0;
+        List<User> users = new List<User>();
+        int numberOfClientsYouNeedToConnect;
+
+        public int NumberOfClientsYouNeedToConnect {
+            get { return numberOfClientsYouNeedToConnect; }
+            set { numberOfClientsYouNeedToConnect = value; }
+        }
+
+        public void PlayGame()
+        {
+            //TcpListener tcpListener = new TcpListener(10);
+            User.startListener();
+
+
+            for (int i = 0; i < numberOfClientsYouNeedToConnect; i++)
+            {
+                User user = new User();
+                user.Name = i + "";
+                user.startUser();
+                users.Add(user);
+                Thread newThread = new Thread(new ThreadStart(user.Listeners));
+                newThread.Start();
+            }
+
+
+            int counter = 0;
+            while (true)
+            {
+
+
+                if (counter > 20000000)
+                {
+                    counter = 0;
+                    Console.WriteLine("userCounter: " + userCounter);
+                    foreach (User user in users)
+                    {
+                        Console.WriteLine(user.Name + " : " + user.Answer);
+
+                    }
+                    userCounter++;
+                }
+
+                if (userCounter == users.Count)
+                {
+                    userCounter = 0;
+                }
+
+                foreach (User user in users)
+                {
+                    if (users[userCounter].Name == user.Name)
+                    {
+                        user.Message = user.Name + " Tev jazime!";
+                    }
+                    else
+                    {
+                        user.Message = "Tagad zime " + users[userCounter].Name + ", kas ir uzzimejis: " + users[userCounter].Answer;
+                    }
+
+                }
+
+                counter++;
+
+
+
+            }
+        }
+    }
+}
---
+++ Server/Server/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    class User
+    {
+        static TcpListener tcpListener = new TcpListener(10);
+        string message = "";
+        string answer = "";
+        string name = "";
+        bool isConnected = false;
+        Socket socketForClient;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public string Answer
+        {
+            get { return answer; }
+            set { answer = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public void setMessage(string message)
+        {
+            this.message = message;
+        }
+
+       public void Listeners()
+        {
+            
+            if (socketForClient.Connected)
+            {
+                Console.WriteLine("Client:" + socketForClient.RemoteEndPoint + " now connected to server.");
+                isConnected = true;
+                NetworkStream networkStream = new NetworkStream(socketForClient);
+                System.IO.StreamWriter streamWriter =
+                new System.IO.StreamWriter(networkStream);
+                System.IO.StreamReader streamReader =
+                new System.IO.StreamReader(networkStream);
+
+                name = streamReader.ReadLine();
+                while (true)
+                {
+                    streamWriter.WriteLine(message);
+                    streamWriter.Flush();
+                    answer = streamReader.ReadLine();
+                    Thread.Sleep(1000);
+                }
+                streamReader.Close();
+                networkStream.Close();
+                streamWriter.Close();
+
+            }            
+
+        }
+
+        public void startUser()
+        {
+            socketForClient = tcpListener.AcceptSocket();
+        }
+
+        public void stopUser()
+        {
+            socketForClient.Close();
+        }
+
+        public static void startListener()
+        {
+            tcpListener.Start();
+        }
+    }
+}
